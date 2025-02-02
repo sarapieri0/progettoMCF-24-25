539a68c0d8f87d70dfb848fe533a336155043611
sarapieri0 <sara_pieri@LAPTOP-AF6RR3GQ.>
2025-02-02 16:05:45 +0100

    Caricamento file

--- modulo_rw.py
+++ modulo_rw.py
@@ -3,13 +3,13 @@ import numpy as np
 #PRIMA CONFIGURAZIONE
 def rw_origine(step0, N, x0, g):
     '''
-    funzione che incrementa il passo del
+    Funzione che incrementa il passo del
     random walk di un fattore costante g, ogni volta che la particella test
     passa per l'origine (da x>0 a x<0 e viceversa), e restituisca
     posizione e lunghezza del passo finale dopo il numero deciso di N passi'''
     if x0==0:
-        x0=int(step0)
-    deltax=np.empty(0)
+        x0=1
+    deltax=[]
     x=x0
     check=np.random.random(N) #array di numeri distribuiti normalmente in (0,1)
     step=step0
@@ -19,32 +19,31 @@ def rw_origine(step0, N, x0, g):
             x=x+step
         else:
             x=x-step
-        deltax=np.append(deltax, x)
+        deltax.append(x)
 
         '''
         condizioni per la modifica del passo in corrispondenza dell'origine'''
         if(count>0):
-            if ((x>0) and (deltax[count-1]<0)):
+            if ((x*deltax[count-1])<0):
                 step=g*step
-            elif ((x<0) and (deltax[count-1]>0)):
-                step=g*step 
         count+=1
-    return deltax, step
+    return np.array(deltax), step
 
 
 #SECONDA CONFIGURAZIONE
 def rw_riflessione(step0, N, x0, g, xmax, b):
     '''
     Funzione che incrementa il passo del random walk (accelerazione) e inverte la direzione
-    della particella (riflessione) ogni volta che attraversa l'origine o la barriera xmax.
+    della particella (riflessione) ogni volta che attraversa l'origine,mentre in corrispondenza della barriera xmax
+    la particella è solo riflessa.
     La barriera decresce costantemente con passo b.
     '''
     if x0==0:
-        x0=int(step0)
-    deltax = [x0]  # Lista per tracciare le posizioni
-    mas = [xmax]   # Lista per tracciare i valori di xmax
-    x = x0         # Posizione iniziale
-    step = step0   # Passo iniziale
+        x0=1
+    deltax = [x0]  
+    mas = [xmax]   
+    x = x0        
+    step = step0 
     i = 0
     
     check = np.random.random(N)
@@ -88,14 +87,14 @@ def rw_accelerazione(step0, N, x0, g, xmax, b):
     passo determinato (b)'''
     
     if(x0==0):
-        x0=int(step0)
+        x0=1
     deltax=[x0]
     x=x0
     step=step0
     i=0
     check=np.random.random(N)
     mas=[xmax]
-    while(i<N-1): #arriva ad N-1 perché il primo ciclo è già riempito da xmax e x0
+    while(i<N-1):
         if ((x<0)or(x>=xmax)):
             if((x<0)and(deltax[i-1]>0)): #passaggio attraverso l'origine
                 x=x+step
